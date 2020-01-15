7156dc6e9515134933eaa22f4b5b3dcf41294771
Egorov, Stas (se6518) <se6518@att.com>
2020-01-14 19:33:00 -0800

    backup service for mac

---
+++ mac/rsync.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+rsync -tmva ~/Downloads /Volumes/rs_hdd/
+rsync -tmva ~/Documents /Volumes/rs_hdd/
+rsync -tmva ~/projects /Volumes/rs_hdd/
+rsync -tmva ~/Pictures /Volumes/rs_hdd/
+rsync -tmva ~/Music /Volumes/rs_hdd/
+rsync -tmva ~/Movies /Volumes/rs_hdd/
+rsync -tmva ~/VirtualBox* /Volumes/rs_hdd/
+rsync -tmva ~/PycharmProjects /Volumes/rs_hdd/
