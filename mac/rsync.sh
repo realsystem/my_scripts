451fc2f983a0061926148cd1c042c2fd7a06a293
Egorov, Stas (se6518) <se6518@att.com>
2020-01-16 15:45:53 -0800

    delete removed files from backup

--- mac/rsync.sh
+++ mac/rsync.sh
@@ -2,11 +2,11 @@
 
 logfile=/tmp/$$.log
 exec > $logfile 2>&1
-rsync -tmva ~/Downloads /Volumes/rs_hdd/
-rsync -tmva ~/Documents /Volumes/rs_hdd/
-rsync -tmva ~/projects /Volumes/rs_hdd/
-rsync -tmva ~/Pictures /Volumes/rs_hdd/
-rsync -tmva ~/Music /Volumes/rs_hdd/
-rsync -tmva ~/Movies /Volumes/rs_hdd/
-rsync -tmva ~/VirtualBox* /Volumes/rs_hdd/
-rsync -tmva ~/PycharmProjects /Volumes/rs_hdd/
+rsync -tmva --delete ~/Downloads /Volumes/rs_hdd/
+rsync -tmva --delete ~/Documents /Volumes/rs_hdd/
+rsync -tmva --delete ~/projects /Volumes/rs_hdd/
+rsync -tmva --delete ~/Pictures /Volumes/rs_hdd/
+rsync -tmva --delete ~/Music /Volumes/rs_hdd/
+rsync -tmva --delete ~/Movies /Volumes/rs_hdd/
+rsync -tmva --delete ~/VirtualBox* /Volumes/rs_hdd/
+rsync -tmva --delete ~/PycharmProjects /Volumes/rs_hdd/
